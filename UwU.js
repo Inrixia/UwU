9501d80675d8f1559a104664dbc05f412dd59c4c
Inrix <Inrixia@gmail.com>
2020-08-10 21:06:43 +1200

    Its Time

--- UwU.js
+++ UwU.js
@@ -1,7 +1,5 @@
 const translate = require('./translate.js')
 
-console.log(translate("can you rub my belly"))
-
 const discordjs = require("discord.js")
 const config = require("./config.json")
 
@@ -12,30 +10,27 @@ const discord = new discordjs.Client()
  */
 const hooks = new discordjs.Collection()
 
-discord.on("ready", () => {
-    console.log("Hewwo wowld!!")
-})
+discord.on("ready", () => console.log("Hewwo wowld!!"))
 
 
 discord.on("message", async msg => {
-    if (msg.author.id == discord.user.id) return;
+    if (msg.author.id === discord.user.id) return;
     if (msg.author.bot) return;
-    if (!msg.guild) return;
-
-    if (msg.channel.id == config.askChannel) {
-        if (!hooks.has(msg.channel)) hooks.set(msg.channel, await msg.channel.createWebhook("OwO", {reason: "Y E S"}))
-        hooks.get(msg.channel).send(msg.content, {
+	if (!msg.guild) return;
+    if (config.sadID == msg.author.id) {
+		if (!hooks.has(msg.channel)) hooks.set(msg.channel, await msg.channel.createWebhook("OwO", { reason: "Y E S" }))
+        hooks.get(msg.channel).send(translate(msg.content), {
             username: msg.guild.member(msg.author).nickname||msg.author.username,
             avatarURL: msg.author.avatarURL()
-        })
+		})
+		msg.delete();
     }
 })
 
 process.on("SIGINT", () => {
-    hooks.forEach(hook => {
-        hook.delete("No more owo")
-    })
-    console.log("Hook deletion finished")
+    hooks.map(hook => hook.delete("No more owo"))
+	console.log("Hook deletion finished")
+	process.exit(1)
 })
 
 discord.login(config.token)
